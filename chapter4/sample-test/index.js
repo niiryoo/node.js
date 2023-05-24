f7dbfca4b4d0312e512188580aa73b050a89a660
YOOJIIN <yoojiin0328@naver.com>
2023-05-24 10:26:20 +0900

    230524 commit

---
+++ chapter4/sample-test/index.js
@@ -0,0 +1,9 @@
+const calc = require("sample-package"); // 1. sample-package 불러오기
+
+const a = 17;
+const b = 3;
+
+console.log(calc.add(a,b));
+console.log(calc.sub(a,b));
+console.log(calc.multi(a,b));
+console.log(calc.div(a,b));
