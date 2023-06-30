0c375060ff2b020e36511ae80e0cc4147b705eb2
YOOJIIN <yoojiin0328@naver.com>
2023-06-30 16:16:56 +0900

    [NestJS Blog Project] 기존 blog.service.ts에 비즈니스 로직을 구현한 것을 blog.repository.ts로 옮겨놓았고, 서비스 파일은 리포지토리를 사용하는 단순 로직으로 변경.

---
+++ chapter8/blog/src/blog.repository.ts
@@ -0,0 +1,57 @@
+import {readFile, writeFile} from 'fs/promises'; // 1. 파일을 읽고 쓰는 모듈 import
+import { PostDto } from './blog.model';
+import { read } from 'fs';
+import { promises } from 'dns';
+
+// 2. 블로그 리포지토리 인터페이스 정의
+export interface BlogRepository{
+    getAllPost(): Promise<PostDto[]>;
+    createPost(postDto: PostDto);
+    getPost(id: String): Promise<PostDto>;
+    deletePost(id:String);
+    updatePost(id:String, postDto: PostDto);
+}
+
+// 3. BlogRepository를 구현한 클래스. 파일을 읽고 쓰기
+export class BlogFileRepository implements BlogRepository{
+    FILE_NAME = './src/blog.data.json';
+
+    // 4. 파일을 읽어서 모든 게시글 불러오기
+    async getAllPost(): Promise<PostDto[]> {
+        const datas = await readFile(this.FILE_NAME, 'utf8');
+        const posts = JSON.parse(datas);
+        return posts;
+    }
+
+    // 5. 게시글 쓰기
+    async createPost(postDto: PostDto){
+        const posts = await this.getAllPost();
+        const id = posts.length + 1;
+        const createPost = {id: id.toString(), ...postDto, createdDt: new Date()};
+        posts.push(createPost);
+        await writeFile(this.FILE_NAME, JSON.stringify(posts));
+    }
+
+    // 6. 게시글 하나 가져오기
+    async getPost(id: string): Promise<PostDto> {
+        const posts = await this.getAllPost();
+        const result = posts.find((post) => post.id === id);
+        return result;
+    }
+
+    // 7. 게시글 하나 삭제
+    async deletePost(id: String) {
+        const posts = await this.getAllPost();
+        const filteredPosts = posts.filter((post) => post.id !== id);
+        await writeFile(this.FILE_NAME, JSON.stringify(filteredPosts));
+    }
+
+    // 8. 게시글 하나 수정하기
+    async updatePost(id: string, postDto: PostDto){
+        const posts = await this.getAllPost();
+        const index = posts.findIndex((post) => post.id === id);
+        const updatePost = {id, ...postDto, updatedDt: new Date()};
+        posts[index] = updatePost;
+        await writeFile(this.FILE_NAME, JSON.stringify(posts));
+    }
+}
--- chapter8/blog/src/blog.service.ts
+++ chapter8/blog/src/blog.service.ts
@@ -1,35 +1,32 @@
 import { PostDto } from "./blog.model"; // 1. 게시글의 타입 정보 import
+import { BlogFileRepository, BlogRepository } from "./blog.repository";
 
 export class BlogService { 
     posts = []; // 2. 게시글 배열 선언
+    blogRepository: BlogRepository;
 
-    getAllPosts(){ // 3. 모든 게시글 가져오기
-        return this.posts;
+    constructor(){
+        this.blogRepository = new BlogFileRepository();
+    }
+
+     async getAllPosts(){ // 3. 모든 게시글 가져오기
+        return await this.blogRepository.getAllPost();
     }
 
     createPost(postDto : PostDto){ // 4. 게시글 작성
-        const id = this.posts.length + 1;
-        this.posts.push({ id: id.toString(), ...postDto, createdDt: new Date()});
+        this.blogRepository.createPost(postDto);
     }
 
-    getPost(id){ // 5. 게시글 하나 가져오기
-        const post = this.posts.find((post) => {
-            return post.id === id;
-        });
-        console.log(post);
-        return post;
+    async getPost(id){ // 5. 게시글 하나 가져오기
+        return await this.blogRepository.getPost(id);
     }
 
     delete(id){ // 6. 게시글 삭제
-        const filteredPosts = this.posts.filter((post) => post.id !== id);
-        this.posts = [...filteredPosts];
+        this.blogRepository.deletePost(id);
     }
 
     updatePost(id, postDto: PostDto){ // 7. 게시글 업데이트
-        let updateIndex = this.posts.findIndex((post) => post.id === id);
-        const updatePost = {id, ...postDto, updateDt: new Date()};
-        this.posts[updateIndex] = updatePost;
-        return updatePost;
+        this.blogRepository.updatePost(id, postDto);
 
     }
 }
