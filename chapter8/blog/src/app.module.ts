ee0c025cdccbdba5195658c5c3f1e9e34e4f57a2
YOOJIIN <yoojiin0328@naver.com>
2023-07-03 13:38:12 +0900

    [NestJS Blog 프로젝트] 몽고디비 연결 설정과 프로바이더 설정 추가

--- chapter8/blog/src/app.module.ts
+++ chapter8/blog/src/app.module.ts
@@ -1,11 +1,21 @@
-import { BlogFileRepository } from './blog.repository';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BlogController } from './blog.controller';
+import { BlogFileRepository, BlogMongoRepository } from './blog.repository';
+import { Blog, BlogSchema} from './blog.schema';
 import { BlogService } from './blog.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // 1. 몽고디비 연결 설정
+    MongooseModule.forRoot(
+      'mongodb+srv://yoojiin0328:dbwldls0328@cluster0.xkcgvka.mongodb.net/blog',
+    ),
+    // 2. 몽고디비 스키마 설정
+    MongooseModule.forFeature([{name: Blog.name, schema: BlogSchema}]),
+  ],
   controllers: [BlogController],
-  providers: [BlogService, BlogFileRepository],
+  // 3. 프로바이더 설정 = 다른 곳에서 의존성 주입
+  providers: [BlogService, BlogFileRepository, BlogMongoRepository],
 })
 export class AppModule {}
