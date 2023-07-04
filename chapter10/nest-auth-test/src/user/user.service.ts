213f69e57e18a3f63b7ee1271d815e252c08fb77
YOOJIIN <yoojiin0328@naver.com>
2023-07-04 14:05:44 +0900

    [회원가입과 인증] 1. entity 생성 2. userService 생성 3. userController 생성 4. 위에 만든 entity, service, controller을 user.module과 app.module에 설정 5. test 진행 (user.http)

--- chapter10/nest-auth-test/src/user/user.service.ts
+++ chapter10/nest-auth-test/src/user/user.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm'; // 리포지토리 의존성 주입 데코레이터
+import { User } from './user.entity';
+import { Repository } from 'typeorm'; // typeorm의 리포지토리. 저장, 읽기 같은 기본적인 메서드들 제공
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(User) private userRepository: Repository<User>,
+    ) {}
+
+    createUser(user) : Promise<User>{
+        return this.userRepository.save(user);
+    }
+
+    async getUser(email: string){
+        const result = this.userRepository.findOne({
+            where: {email},
+        });
+        return result;
+    }
+
+    async updateUser(email: string, _user){
+        const user: User = await this.getUser(email);
+        console.log(user);
+
+        user.username = _user.username;
+        user.password = _user.password;
+        console.log(user);
+
+        this.userRepository.save(user);
+    }
+
+    deleteUser(email: any){
+        return this.userRepository.delete({email});
+    }
+}
