bad82685c4cfc79747836b82b4e3ed237fdf4e6c
YOOJIIN <yoojiin0328@naver.com>
2023-07-05 15:01:25 +0900

    로그인에 사용할 가드(LoginAuthGuard)와 로그인 후 인증에 사용할 가드(AuthenticatedGuard) 생성.
    LoginAuthGuard: HTTP 요청으로 받은 email과 password 정보로 유효한 user 정보가 있는지 확인해, 유효할 경우 유저의 정보를 세션에 저장.
    AuthenticatedGuard는 HTTP 요청에 있는 쿠키를 찾아 쿠키에 있는 정보로 세션을 확인해 로그인이 완료된 사용자인지 판별.

--- chapter10/nest-auth-test/src/auth/auth.guard.ts
+++ chapter10/nest-auth-test/src/auth/auth.guard.ts
@@ -1,5 +1,7 @@
-import {CanActivate, Injectable} from '@nestjs/common';
+import {CanActivate, ExecutionContext, Injectable} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class LoginGuard implements CanActivate {
@@ -29,5 +31,24 @@ export class LoginGuard implements CanActivate {
 
         request.user = user;
         return true;
+    }
+}
+
+@Injectable()
+export class LocalAuthGuard extends AuthGuard('local'){ // AuthGuard 상속
+    async canActivate(context: any): Promise<boolean>{
+        const result = (await super.canActivate(context)) as boolean;
+        //로컬 strategy 실행
+        const request = context.switchToHttp().getRequest();
+        await super.logIn(request); // 세션 저장
+        return result;
+    }
+}
+
+@Injectable()
+export class AuthenticatedGuard implements CanActivate{
+    canActivate(context: ExecutionContext): boolean {
+        const request = context.switchToHttp().getRequest();
+        return request.isAuthenticated(); // 세션에서 정보를 읽어서 인증 확인
     }
 }
