125f4581c594a30690b00dcca2982a1dc42422a0
YOOJIIN <yoojiin0328@naver.com>
2023-07-05 17:50:57 +0900

    password가 없는 때도 데이터가 저장되는 기능과 구글 인증 시의 식별자인 providerId 추가. (자세한 거 주석 참고)

--- chapter10/nest-auth-test/src/user/user.entity.ts
+++ chapter10/nest-auth-test/src/user/user.entity.ts
@@ -8,12 +8,15 @@ export class User{
     @Column({unique: true})
     email: string;
 
-    @Column()
+    @Column({nullable: true}) //패스워드에 빈 값 허용: 구글 OAuth 인증에는 구글에서 인증을 한 다음 돌아오므로 패스워드를 알 수 없기 떄문
     password: string;
 
     @Column()
     username: string;
 
-    @Column({default: true})
-    createdDt: Date = new Date();
+    @Column({type: 'datetime', default: () => 'CURRENT_TIMESTAMP'})
+    createdDt: Date;
+
+    @Column({nullable: true}) // providerId에 빈 값 허용: OAuth로 가입하지 않은 경우에는 모르는 값이기에 빈 값 허용
+    providerId: string; // providerId 추가: OAuth 인증 시 식별자로 사용할 수 있는 값
 }
